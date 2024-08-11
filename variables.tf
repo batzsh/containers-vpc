5d1293cd6a2cb3a60182f6c13ca48356952ea934
Henrique Balmant <henrique.balmant@gmail.com>
2024-08-11 12:00:42 -0300

    feat: add variables, providers and backend init

---
+++ variables.tf
@@ -0,0 +1,9 @@
+### GENERAL CONFIGS ###
+
+variable "project_name" {
+  description = "Project name performs as a prefix at all resources from this project"
+}
+
+variable "region" {
+  default = "AWS Region where resources will be created"
+}
